07d327406a75fcd461c524634689e73474106766
Raghu Betina <raghu@firstdraft.com>
2017-08-15 14:58:49 +0000

    Whitelist columns in Waypoint for ActiveAdmin

--- app/admin/waypoint.rb
+++ app/admin/waypoint.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Waypoint do
 
+ permit_params :business_id, :latitude, :longitude, :route_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
