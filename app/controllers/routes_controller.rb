fc68448c0916c7a86ee855ffef1d325804d9d161
Raghu Betina <raghu@firstdraft.com>
2017-08-15 14:58:49 +0000

    Only route user can modify route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,4 +1,14 @@
 class RoutesController < ApplicationController
+  before_action :current_user_must_be_route_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_route_user
+    route = Route.find(params[:id])
+
+    unless current_user == route.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @routes = Route.all
 
